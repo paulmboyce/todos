a364cd92e64b36934c9603eb8032bb57b15b0abf
paulmboyce <paul.boyce@goobito.com>
2020-02-13 17:06:39 +0000

    Eslint installed and running. 'Standard' formattng applied

--- todoList.js
+++ todoList.js
@@ -1,170 +1,165 @@
 // const
-const NOT_DONE = '&nbsp;.&nbsp;';
-const DONE = '&#10003;';
-
+const NOT_DONE = '&nbsp;.&nbsp;'
+const DONE = '&#10003;'
 
 // Context defence for testing outwith broswer.
-if (typeof window === "undefined") { 
-  var window = {};
+// eslint-disable-next-line no-use-before-define
+if (typeof window === 'undefined') {
+  var window = {}
 }
 
-window._app = {todoList: []};
+window._app = { todoList: [] }
 
 var todoList = {
-  todos: [{text:'todo 1', completed:false},{text:'todo 2', completed:false}],
-  
-  display: function() {
-    var message = buildMessage(this.todos);
-    printTo('output', 'My Todos:' + message); 
-    // Do on mass because we create the elements on mass. 
-    // When code changes to append individual elements, 
-    // we would do this for that element only. 
-    renderTodosWithEventHandling(this); 
-  }, 
+  todos: [{ text: 'todo 1', completed: false }, { text: 'todo 2', completed: false }],
+
+  display: function () {
+    var message = buildMessage(this.todos)
+    printTo('output', 'My Todos:' + message)
+    // Do on mass because we create the elements on mass.
+    // When code changes to append individual elements,
+    // we would do this for that element only.
+    renderTodosWithEventHandling(this)
+  },
   add: function (todo) {
-  
-    if (typeof todo != 'string') {
-      return ('oops should be a string');
+    if (typeof todo !== 'string') {
+      return ('oops should be a string')
     }
-     var newTodo = {
-      text:todo, 
-      completed:false
-    };
+    var newTodo = {
+      text: todo,
+      completed: false
+    }
 
-    this.todos.push (newTodo);
-    this.display();
-    return  newTodo;
+    this.todos.push(newTodo)
+    this.display()
+    return newTodo
   },
-  change: function(position, newText) {
-    this.todos[position].text = newText;
-    this.display();
+  change: function (position, newText) {
+    this.todos[position].text = newText
+    this.display()
   },
-  delete: function(position) {
-    this.todos.splice(position,1);
-    this.display();
-
+  delete: function (position) {
+    this.todos.splice(position, 1)
+    this.display()
   },
-  toggleCompleted: function(position) {
-    var todo = this.todos[position];
-    todo.completed = !todo.completed;
-    this.display();
+  toggleCompleted: function (position) {
+    var todo = this.todos[position]
+    todo.completed = !todo.completed
+    this.display()
   },
-  toggleAllCompleted: function(done) {
-    var numTodos = this.todos.length;
+  toggleAllCompleted: function (done) {
+    var numTodos = this.todos.length
     for (var i = 0; i < numTodos; i++) {
-      this.todos[i].completed = done;
+      this.todos[i].completed = done
     }
   },
   // Set ALL as COMPLETE (true), unless ALL are complete (so set as false)
-  toggleAll: function() {
-
-    var hasAnyIncomplete = false;  
-    var numTodos = this.todos.length;
+  toggleAll: function () {
+    var hasAnyIncomplete = false
+    var numTodos = this.todos.length
 
     // If there is even one thats completed, mark ALL as complete.
     for (var i = 0; i < numTodos; i++) {
       if (this.todos[i].completed === false) {
-        hasAnyIncomplete = true;
-        break;
+        hasAnyIncomplete = true
+        break
       }
     }
     // Otherwise mark all incomplete
     if (hasAnyIncomplete === true) {
-      this.toggleAllCompleted(true);
-    } 
-    else {
-      this.toggleAllCompleted(false);
+      this.toggleAllCompleted(true)
+    } else {
+      this.toggleAllCompleted(false)
     }
     // Update UI.
-    this.display();
+    this.display()
   }
 
 }
-window._app.todoList = todoList;
+window._app.todoList = todoList
 
-function renderTodosWithEventHandling(todoList) {
-  var elements = buildElements(todoList.todos);
+function renderTodosWithEventHandling (todoList) {
+  var elements = buildElements(todoList.todos)
   if (window && window.document) {
-    window.document.querySelector('div#id-todos').innerHTML = elements;
+    window.document.querySelector('div#id-todos').innerHTML = elements
   }
-  addClickEventHandlersToToggleButtons(todoList);
+  addClickEventHandlersToToggleButtons(todoList)
 }
 
 function buildMessage (todos) {
-  var message = '';
-  var numTodos = todos.length;
+  var message = ''
+  var numTodos = todos.length
 
   if (numTodos < 1) {
-    return message = "You have no outstanding Todos!"
+    message = 'You have no outstanding Todos!'
+    return message
   }
 
-  for (var i = 0; i < numTodos ; i++) {
-    message = '\n'+checkCompleted(todos[i])+' '+todos[i].text+message;  
+  for (var i = 0; i < numTodos; i++) {
+    message = '\n' + checkCompleted(todos[i]) + ' ' + todos[i].text + message
   }
-  return message;
+  return message
 }
 
 function buildElements (todos) {
-  var numTodos = todos.length;
-  var html = '';
-  for (var i=0;i<numTodos; i++) {
-    var element = '<button type="button" item="' +i+'" class="btn btn-small btn-info toggle">' + checkCompleted(todos[i]) + '</BUTTON>';
-    element += '<span>'+ todos[i].text+'</span>';
-    element += '<span item="' +i+'" class="delete" onclick=\'deleteUITodo(this);\'> X</span>';
-    element += '<br/>';
-    html = element + html; // prepend
+  var numTodos = todos.length
+  var html = ''
+  for (var i = 0; i < numTodos; i++) {
+    var element = '<button type="button" item="' + i + '" class="btn btn-small btn-info toggle">' + checkCompleted(todos[i]) + '</BUTTON>'
+    element += '<span>' + todos[i].text + '</span>'
+    element += '<span item="' + i + '" class="delete" onclick=\'deleteUITodo(this);\'> X</span>'
+    element += '<br/>'
+    html = element + html // prepend
   }
-  return html;
+  return html
 }
 
-
-function checkCompleted(todo) {
-  var done = NOT_DONE;
+function checkCompleted (todo) {
+  var done = NOT_DONE
   if (todo.completed === true) {
-    done = DONE;
+    done = DONE
   }
-  return done;
+  return done
 }
 
 function toggleUIElement (el, todoList) {
-  var position = el.getAttribute('item');
-  todoList.toggleCompleted(position);
-  todoList.display();
+  var position = el.getAttribute('item')
+  todoList.toggleCompleted(position)
+  todoList.display()
 }
 
+// This is called from dynamically HTML code.
+// eslint-disable-next-line no-unused-vars
 function deleteUITodo (el) {
-  var position = el.getAttribute('item');
-  todoList.delete(position);
+  var position = el.getAttribute('item')
+  todoList.delete(position)
 }
 
-function printTo(id, newText) {
-
+function printTo (id, newText) {
   // Validate web context exists.Else default to console
-  if (window.document) { 
-    var target =  window.document.querySelector('#' + id);
-    target.innerHTML= newText ;
-  } 
-}   
-
-function addClickEventHandlersToToggleButtons(todoList){
-  document.querySelectorAll('button.toggle')
-  .forEach(function (element){ 
-    element.addEventListener('click', function(){toggleUIElement(this, todoList);});
-  });
+  if (window.document) {
+    var target = window.document.querySelector('#' + id)
+    target.innerHTML = newText
+  }
 }
 
+function addClickEventHandlersToToggleButtons (todoList) {
+  document.querySelectorAll('button.toggle')
+    .forEach(function (element) {
+      element.addEventListener('click', function () { toggleUIElement(this, todoList) })
+    })
+}
 
-window._app.todoList.display();   
-window._app.todoList.add("buy milk");
-window._app.todoList.add("buy veggies");
+window._app.todoList.display()
+window._app.todoList.add('buy milk')
+window._app.todoList.add('buy veggies')
 
-
-if (typeof module != "undefined") {
+if (typeof module !== 'undefined') {
   module.exports = {
-    todoList:       todoList,
-    buildMessage:   buildMessage,
+    todoList: todoList,
+    buildMessage: buildMessage,
     checkCompleted: checkCompleted,
-    constants:      {DONE, NOT_DONE},
-    toggleUIElement:  toggleUIElement
-  };
+    constants: { DONE, NOT_DONE },
+    toggleUIElement: toggleUIElement
+  }
 }
--- todoList.test.js
+++ todoList.test.js
@@ -1,192 +1,169 @@
-const TODO = require('./todoList');
-const todoList =  TODO.todoList;
+const TODO = require('./todoList')
+const todoList = TODO.todoList
 
 test('can add a todo', () => {
+  const numTodos = todoList.todos.length
 
-  const numTodos = todoList.todos.length;
-
-  var result = todoList.add("Buy Coffee at Starbucks");
+  var result = todoList.add('Buy Coffee at Starbucks')
 
   // validate list has grown by 1
-  expect(todoList.todos.length).toBe(numTodos +1);
+  expect(todoList.todos.length).toBe(numTodos + 1)
 
   // validate new object props
-  expect(result).toEqual ({text:"Buy Coffee at Starbucks", completed:false});
-});
-
-
+  expect(result).toEqual({ text: 'Buy Coffee at Starbucks', completed: false })
+})
 
 test('all todos are not completed', () => {
-  var todos = todoList.todos;
   for (let index = 0; index < todoList.length; index++) {
-    const todo = todoList[index];
-    expect(todo.completed).toEqual (false);
+    const todo = todoList[index]
+    expect(todo.completed).toEqual(false)
   }
-});
+})
 
 test('can toggle completed', () => {
+  todoList.todos[0].completed = false
+  todoList.toggleCompleted(0)
+  expect(todoList.todos[0].completed).toBe(true)
+  todoList.toggleCompleted(0)
+  expect(todoList.todos[0].completed).toBe(false)
+})
 
-  todoList.todos[0].completed = false;
-  todoList.toggleCompleted(0);
-  expect(todoList.todos[0].completed).toBe(true);
-  todoList.toggleCompleted(0);
-  expect(todoList.todos[0].completed).toBe(false);
-
-});
-
-test ('build message works', () => {
+test('build message works', () => {
   // Lets test for empty
-  todoList.todos = [];
-  expect(TODO.buildMessage(todoList.todos)).toBe("You have no outstanding Todos!");
+  todoList.todos = []
+  expect(TODO.buildMessage(todoList.todos)).toBe('You have no outstanding Todos!')
 
-  todoList.add("todo 1");
-  expect(todoList.todos.length).toBe(1);
+  todoList.add('todo 1')
+  expect(todoList.todos.length).toBe(1)
 
-  expect(TODO.buildMessage(todoList.todos)).toContain("todo 1");
-  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining(TODO.constants.NOT_DONE + " todo 1"));
-  expect(TODO.buildMessage(todoList.todos)).toEqual("\n" + TODO.constants.NOT_DONE + " todo 1");
+  expect(TODO.buildMessage(todoList.todos)).toContain('todo 1')
+  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining(TODO.constants.NOT_DONE + ' todo 1'))
+  expect(TODO.buildMessage(todoList.todos)).toEqual('\n' + TODO.constants.NOT_DONE + ' todo 1')
 
-  todoList.toggleCompleted(0);
-  expect(TODO.buildMessage(todoList.todos)).toEqual("\n" + TODO.constants.DONE + " todo 1");
-  todoList.toggleCompleted(0);
+  todoList.toggleCompleted(0)
+  expect(TODO.buildMessage(todoList.todos)).toEqual('\n' + TODO.constants.DONE + ' todo 1')
+  todoList.toggleCompleted(0)
 
-  todoList.add("todo 2");
-  expect(todoList.todos.length).toBe(2);
+  todoList.add('todo 2')
+  expect(todoList.todos.length).toBe(2)
 
-  expect(TODO.buildMessage(todoList.todos)).toContain(TODO.constants.NOT_DONE + " todo 2");
-  expect(TODO.buildMessage(todoList.todos)).toMatch(TODO.constants.NOT_DONE + " todo 2");
-  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining("" + TODO.constants.NOT_DONE + " todo 2"));
+  expect(TODO.buildMessage(todoList.todos)).toContain(TODO.constants.NOT_DONE + ' todo 2')
+  expect(TODO.buildMessage(todoList.todos)).toMatch(TODO.constants.NOT_DONE + ' todo 2')
+  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining('' + TODO.constants.NOT_DONE + ' todo 2'))
 
-  todoList.toggleCompleted(1); 
-  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining("\n" + TODO.constants.DONE + " todo 2"));
-  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining("\n" + TODO.constants.NOT_DONE + " todo 1"));
+  todoList.toggleCompleted(1)
+  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining('\n' + TODO.constants.DONE + ' todo 2'))
+  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining('\n' + TODO.constants.NOT_DONE + ' todo 1'))
 
   // Check that new items are PRE-Pended
-  expect(TODO.buildMessage(todoList.todos)).toEqual("\n&#10003; todo 2\n" + TODO.constants.NOT_DONE + " todo 1");
+  expect(TODO.buildMessage(todoList.todos)).toEqual('\n&#10003; todo 2\n' + TODO.constants.NOT_DONE + ' todo 1')
 
   // Toggl both items
-  todoList.toggleCompleted(0); 
-  todoList.toggleCompleted(1); 
-  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining("\n&#10003; todo 1"));
-  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining("\n" + TODO.constants.NOT_DONE + " todo 2"));
+  todoList.toggleCompleted(0)
+  todoList.toggleCompleted(1)
+  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining('\n&#10003; todo 1'))
+  expect(TODO.buildMessage(todoList.todos)).toEqual(expect.stringContaining('\n' + TODO.constants.NOT_DONE + ' todo 2'))
 
-  // crrect order  
-  expect(TODO.buildMessage(todoList.todos)).toEqual("\n" + TODO.constants.NOT_DONE + " todo 2\n&#10003; todo 1");
+  // crrect order
+  expect(TODO.buildMessage(todoList.todos)).toEqual('\n' + TODO.constants.NOT_DONE + ' todo 2\n&#10003; todo 1')
 
   // Ok lets delete 2 items and verfy for empty
-  todoList.delete(0);
-  expect(todoList.todos.length).toBe(1);
-  todoList.delete(0);
-  expect(todoList.todos.length).toBe(0);
-  expect(TODO.buildMessage(todoList.todos)).toBe("You have no outstanding Todos!");
+  todoList.delete(0)
+  expect(todoList.todos.length).toBe(1)
+  todoList.delete(0)
+  expect(todoList.todos.length).toBe(0)
+  expect(TODO.buildMessage(todoList.todos)).toBe('You have no outstanding Todos!')
+})
 
-
-});
-
-
-test ("Check competed", () => {
-
+test('Check competed', () => {
   // Lets test for empty
   todoList.todos = [
-    {text: 'todo 1', completed: false},
-    {text: 'todo 2', completed: true},
-  ];  
+    { text: 'todo 1', completed: false },
+    { text: 'todo 2', completed: true }
+  ]
 
-  expect(TODO.checkCompleted(todoList.todos[0])).toBe(TODO.constants.NOT_DONE);
-  expect(TODO.checkCompleted(todoList.todos[1])).toBe(TODO.constants.DONE);
+  expect(TODO.checkCompleted(todoList.todos[0])).toBe(TODO.constants.NOT_DONE)
+  expect(TODO.checkCompleted(todoList.todos[1])).toBe(TODO.constants.DONE)
+})
 
-});
-
-test ('toggle all as COMPLETE', () => {
-  todoList.toggleAllCompleted(true);
-  for (var i =0;i< todoList.todos.length; i++) {
-    expect(todoList.todos[i].completed).toBe(true);
+test('toggle all as COMPLETE', () => {
+  todoList.toggleAllCompleted(true)
+  for (var i = 0; i < todoList.todos.length; i++) {
+    expect(todoList.todos[i].completed).toBe(true)
   }
-});
+})
 
-test ('toggle all as NOT COMPLETE', () => {
-  todoList.toggleAllCompleted(false);
-  for (var i =0;i< todoList.todos.length; i++) {
-    expect(todoList.todos[i].completed).toBe(false);
+test('toggle all as NOT COMPLETE', () => {
+  todoList.toggleAllCompleted(false)
+  for (var i = 0; i < todoList.todos.length; i++) {
+    expect(todoList.todos[i].completed).toBe(false)
   }
-});
+})
 
-test ('toggle if ALL are complete toggleAll sets ALL to INCOPLETE', () => {
-
-  todoList.toggleAllCompleted(true);
+test('toggle if ALL are complete toggleAll sets ALL to INCOPLETE', () => {
+  todoList.toggleAllCompleted(true)
   // Expect ALL to be true
-  for (var i =0;i< todoList.todos.length; i++) {
-    expect(todoList.todos[i].completed).toEqual(true);
+  for (let i = 0; i < todoList.todos.length; i++) {
+    expect(todoList.todos[i].completed).toEqual(true)
   }
 
-  todoList.toggleAll();
+  todoList.toggleAll()
 
   // Expect ALL to be false
-  for (var i =0; i< todoList.todos.length; i++) {
-    expect(todoList.todos[i].completed).toEqual(false);
+  for (let i = 0; i < todoList.todos.length; i++) {
+    expect(todoList.todos[i].completed).toEqual(false)
   }
+})
 
-
-
-});
-
-test ('Special Case: toggle all sets to FALSE if everything is true ', ()=> {
-
-  todoList.toggleAllCompleted (true); // Sets all true
+test('Special Case: toggle all sets to FALSE if everything is true ', () => {
+  todoList.toggleAllCompleted(true) // Sets all true
 
   // Toggle all shsoul go true.
-  todoList.toggleAll(); 
+  todoList.toggleAll()
 
   // Expect ALL to be true
-  for (var i =0;i< todoList.todos.length; i++) {
-    expect(todoList.todos[i].completed).toEqual(false);
+  for (var i = 0; i < todoList.todos.length; i++) {
+    expect(todoList.todos[i].completed).toEqual(false)
   }
 
   // if even one os false, all beecome true
 
-
-  todoList.toggleAllCompleted(true); // Sets all true again
+  todoList.toggleAllCompleted(true) // Sets all true again
   // Mark one as incmplete:
-  todoList.todos[0].completed = false;
+  todoList.todos[0].completed = false
 
   // Toggle all shsoul go true.
-  todoList.toggleAll(); 
+  todoList.toggleAll()
   // Expect ALL to be true
-  for (var i =0;i< todoList.todos.length; i++) {
-    expect(todoList.todos[i].completed).toEqual(true);
+  for (let i = 0; i < todoList.todos.length; i++) {
+    expect(todoList.todos[i].completed).toEqual(true)
+  }
+})
+
+test('toggleUI component check use of API, for example ' +
+      'that it calls getAttribute, for "item"' +
+      'much of this is unneccessary because were are testing internal calls rather than funtion effects ' +
+      'but it is an example of using injection of fake (aka MOCK) objects.', () => {
+  const mockFnGetAttribute = jest.fn((param) => {
+    return 0
+  })
+
+  const fakeElement = { getAttribute: mockFnGetAttribute }
+  const mockFnToggleCompleted = jest.fn()
+
+  const mockFnDisplay = jest.fn()
+  const fakeTodoList = {
+    toggleCompleted: mockFnToggleCompleted,
+    display: mockFnDisplay
   }
 
-});
+  TODO.toggleUIElement(fakeElement, fakeTodoList)
 
+  expect(mockFnGetAttribute).toBeCalled()
+  expect(mockFnGetAttribute).toHaveBeenCalledWith('item')
 
-test('toggleUI component check use of API, for example '
-      + 'that it calls getAttribute, for "item"'
-      + 'much of this is unneccessary because were are testing internal calls rather than funtion effects '
-      + 'but it is an example of using injection of fake (aka MOCK) objects.', ()=> {
+  expect(mockFnToggleCompleted).toBeCalled()
+  expect(mockFnToggleCompleted).toHaveBeenCalledWith(0)
 
-  const mockFn_GetAttribute = jest.fn((param)=>{
-    return 0;
-  });
-
-  const fakeElement = {getAttribute: mockFn_GetAttribute};
-  const mockFn_ToggleCompleted = jest.fn();
-        
-  const mockFn_Display = jest.fn();
-  const fakeTodoList = {
-    toggleCompleted: mockFn_ToggleCompleted, 
-    display:         mockFn_Display
-  };
-
-
-  TODO.toggleUIElement (fakeElement, fakeTodoList);
-
-  expect(mockFn_GetAttribute).toBeCalled();
-  expect(mockFn_GetAttribute).toHaveBeenCalledWith('item');
-
-  expect(mockFn_ToggleCompleted).toBeCalled();
-  expect(mockFn_ToggleCompleted).toHaveBeenCalledWith(0);
-
-  expect(mockFn_Display).toBeCalled();
-});
-
-
+  expect(mockFnDisplay).toBeCalled()
+})
