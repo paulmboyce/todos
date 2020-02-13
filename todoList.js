080b19bd032a7d4c9b06f5f7ab4e4b68e34e5916
paulmboyce <paul.boyce@goobito.com>
2020-02-13 20:36:17 +0000

    Updated coe followig AirBnB Linting. Main changes are use of semi-colon, avoid ++, const not var, and scope and shadowing. Learned a TON just by doing this exercise.

--- todoList.js
+++ todoList.js
@@ -1,165 +1,170 @@
+/* eslint-disable block-scoped-var */
+/* eslint-disable no-var */
+/* eslint-disable no-use-before-define */
 // const
-const NOT_DONE = '&nbsp;.&nbsp;'
-const DONE = '&#10003;'
+const NOT_DONE = '&nbsp;.&nbsp;';
+const DONE = '&#10003;';
 
 // Context defence for testing outwith broswer.
 // eslint-disable-next-line no-use-before-define
+// eslint-disable-next-line block-scoped-var
+// eslint-disable-next-line no-use-before-define
+// eslint-disable-next-line block-scoped-var
 if (typeof window === 'undefined') {
-  var window = {}
+  // eslint-disable-next-line vars-on-top
+  var window = {};
 }
 
-window._app = { todoList: [] }
+// eslint-disable-next-line block-scoped-var
+window.myTodoApp = { todoList: [] };
 
-var todoList = {
+const todoList = {
   todos: [{ text: 'todo 1', completed: false }, { text: 'todo 2', completed: false }],
 
-  display: function () {
-    var message = buildMessage(this.todos)
-    printTo('output', 'My Todos:' + message)
+  display() {
+    const message = buildMessage(this.todos);
+    printTo('output', `My Todos:${message}`);
     // Do on mass because we create the elements on mass.
     // When code changes to append individual elements,
     // we would do this for that element only.
-    renderTodosWithEventHandling(this)
+    renderTodosWithEventHandling(this);
   },
-  add: function (todo) {
+  add(todo) {
     if (typeof todo !== 'string') {
-      return ('oops should be a string')
+      return ('oops should be a string');
     }
-    var newTodo = {
+    const newTodo = {
       text: todo,
-      completed: false
-    }
+      completed: false,
+    };
 
-    this.todos.push(newTodo)
-    this.display()
-    return newTodo
+    this.todos.push(newTodo);
+    this.display();
+    return newTodo;
   },
-  change: function (position, newText) {
-    this.todos[position].text = newText
-    this.display()
+  change(position, newText) {
+    this.todos[position].text = newText;
+    this.display();
   },
-  delete: function (position) {
-    this.todos.splice(position, 1)
-    this.display()
+  delete(position) {
+    this.todos.splice(position, 1);
+    this.display();
   },
-  toggleCompleted: function (position) {
-    var todo = this.todos[position]
-    todo.completed = !todo.completed
-    this.display()
+  toggleCompleted(position) {
+    const todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.display();
   },
-  toggleAllCompleted: function (done) {
-    var numTodos = this.todos.length
-    for (var i = 0; i < numTodos; i++) {
-      this.todos[i].completed = done
+  toggleAllCompleted(done) {
+    const numTodos = this.todos.length;
+    for (let i = 0; i < numTodos; i += 1) {
+      this.todos[i].completed = done;
     }
   },
   // Set ALL as COMPLETE (true), unless ALL are complete (so set as false)
-  toggleAll: function () {
-    var hasAnyIncomplete = false
-    var numTodos = this.todos.length
+  toggleAll() {
+    let hasAnyIncomplete = false;
+    const numTodos = this.todos.length;
 
     // If there is even one thats completed, mark ALL as complete.
-    for (var i = 0; i < numTodos; i++) {
+    for (let i = 0; i < numTodos; i += 1) {
       if (this.todos[i].completed === false) {
-        hasAnyIncomplete = true
-        break
+        hasAnyIncomplete = true;
+        break;
       }
     }
     // Otherwise mark all incomplete
     if (hasAnyIncomplete === true) {
-      this.toggleAllCompleted(true)
+      this.toggleAllCompleted(true);
     } else {
-      this.toggleAllCompleted(false)
+      this.toggleAllCompleted(false);
     }
     // Update UI.
-    this.display()
-  }
+    this.display();
+  },
 
-}
-window._app.todoList = todoList
+};
+// eslint-disable-next-line block-scoped-var
+window.myTodoApp.todoList = todoList;
 
-function renderTodosWithEventHandling (todoList) {
-  var elements = buildElements(todoList.todos)
+function renderTodosWithEventHandling(injectedTodoList) {
+  const elements = buildElements(injectedTodoList.todos);
   if (window && window.document) {
-    window.document.querySelector('div#id-todos').innerHTML = elements
+    window.document.querySelector('div#id-todos').innerHTML = elements;
   }
-  addClickEventHandlersToToggleButtons(todoList)
+  addClickEventHandlersToToggleButtons(injectedTodoList);
 }
 
-function buildMessage (todos) {
-  var message = ''
-  var numTodos = todos.length
+function buildMessage(todos) {
+  let message = '';
+  const numTodos = todos.length;
 
   if (numTodos < 1) {
-    message = 'You have no outstanding Todos!'
-    return message
+    message = 'You have no outstanding Todos!';
+    return message;
   }
 
-  for (var i = 0; i < numTodos; i++) {
-    message = '\n' + checkCompleted(todos[i]) + ' ' + todos[i].text + message
+  for (let i = 0; i < numTodos; i += 1) {
+    message = `\n${checkCompleted(todos[i])} ${todos[i].text}${message}`;
   }
-  return message
+  return message;
 }
 
-function buildElements (todos) {
-  var numTodos = todos.length
-  var html = ''
-  for (var i = 0; i < numTodos; i++) {
-    var element = '<button type="button" item="' + i + '" class="btn btn-small btn-info toggle">' + checkCompleted(todos[i]) + '</BUTTON>'
-    element += '<span>' + todos[i].text + '</span>'
-    element += '<span item="' + i + '" class="delete" onclick=\'deleteUITodo(this);\'> X</span>'
-    element += '<br/>'
-    html = element + html // prepend
+function buildElements(todos) {
+  const numTodos = todos.length;
+  let html = '';
+  for (let i = 0; i < numTodos; i += 1) {
+    let element = `<button type="button" item="${i}" class="btn btn-small btn-info toggle">${checkCompleted(todos[i])}</BUTTON>`;
+    element += `<span>${todos[i].text}</span>`;
+    element += `<span item="${i}" class="delete" onclick='deleteUITodo(this);'> X</span>`;
+    element += '<br/>';
+    html = element + html; // prepend
   }
-  return html
+  return html;
 }
 
-function checkCompleted (todo) {
-  var done = NOT_DONE
+function checkCompleted(todo) {
+  let done = NOT_DONE;
   if (todo.completed === true) {
-    done = DONE
+    done = DONE;
   }
-  return done
+  return done;
 }
 
-function toggleUIElement (el, todoList) {
-  var position = el.getAttribute('item')
-  todoList.toggleCompleted(position)
-  todoList.display()
+function toggleUIElement(el, injectedTodoList) {
+  const position = el.getAttribute('item');
+  injectedTodoList.toggleCompleted(position);
+  injectedTodoList.display();
 }
 
 // This is called from dynamically HTML code.
 // eslint-disable-next-line no-unused-vars
-function deleteUITodo (el) {
-  var position = el.getAttribute('item')
-  todoList.delete(position)
+function deleteUITodo(el) {
+  const position = el.getAttribute('item');
+  todoList.delete(position);
 }
 
-function printTo (id, newText) {
+function printTo(id, newText) {
   // Validate web context exists.Else default to console
   if (window.document) {
-    var target = window.document.querySelector('#' + id)
-    target.innerHTML = newText
+    const target = window.document.querySelector(`#${id}`);
+    target.innerHTML = newText;
   }
 }
 
-function addClickEventHandlersToToggleButtons (todoList) {
+function addClickEventHandlersToToggleButtons(injectedTodoList) {
   document.querySelectorAll('button.toggle')
-    .forEach(function (element) {
-      element.addEventListener('click', function () { toggleUIElement(this, todoList) })
-    })
+    .forEach((element) => {
+      element.addEventListener('click', function handleClickEvent() { toggleUIElement(this, injectedTodoList); });
+    });
 }
-
-window._app.todoList.display()
-window._app.todoList.add('buy milk')
-window._app.todoList.add('buy veggies')
 
 if (typeof module !== 'undefined') {
   module.exports = {
-    todoList: todoList,
-    buildMessage: buildMessage,
-    checkCompleted: checkCompleted,
+    todoList,
+    buildMessage,
+    checkCompleted,
     constants: { DONE, NOT_DONE },
-    toggleUIElement: toggleUIElement
-  }
+    toggleUIElement,
+  };
 }
