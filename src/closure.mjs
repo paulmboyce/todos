406ae8c7a70290adef80d6cc3c93114c873e4b3f
paulmboyce <paul.boyce@goobito.com>
2020-02-28 19:36:46 +0000

    Migrated tests from CommonJS require() to ESM import. Removed --no-cache flag from jest to save recompiling babel everytime (ADDITIONAL FILES + Added --no-error-on-unmatched-pattern flag for ESLint when not finding .js files following migration)

---
+++ src/closure.mjs
@@ -0,0 +1,25 @@
+function range (start, end) {
+    const result = [];
+
+    // Closure lets  remember 'start' in context
+    function runRangeWIthEnd (end2) {
+        return range(start, end2);
+    }
+
+    if (end === undefined) {
+        return runRangeWIthEnd;
+    }
+
+    if (end >= start) {
+        const steps = end - start;
+        for (let i = 0; i <= steps; i += 1) {
+            result.push(start + i);
+        }
+    }
+    return result;
+}
+
+const range1 = range(3);
+range1(3);
+
+export { range };
---
+++ test/closure.test.mjs
@@ -0,0 +1,67 @@
+import { range as Range } from '../src/closure.mjs';
+
+test('returns [3] for range (3,3)', () => {
+    // ARRANGE
+    // ACT
+    const result = Range(3, 3);
+    // ASSERT
+    expect(result[0]).toBe(3);
+});
+
+test('returns [3,4,5,6,7,8] for range (3,8)', () => {
+    // ARRANGE
+    const expected = [3, 4, 5, 6, 7, 8];
+    // ACT
+    const result = Range(3, 8);
+    // ASSERT
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+test('returns [] for range(3,0)', () => {
+    // ARRANGE
+    const expected = [];
+    // ACT
+    const result = Range(3, 0);
+    // ASSERT
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+test('returns [3] for range(3).range(3) ', () => {
+    // ARRANGE
+    const expected = [3];
+    // ACT
+    let result = Range(3);
+    result = result(3);
+    // ASSERT
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+test('returns [3,4,5,6,7,8] for range(3) (8)', () => {
+// ARRANGE
+    const expected = [3, 4, 5, 6, 7, 8];
+    // ACT
+    let result = Range(3);
+    result = result(8);
+    // ASSERT
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+test('returns [] for range(3) (0)', () => {
+    // ARRANGE
+    const expected = [];
+    // ACT
+    let result = Range(3);
+    result = result(0);
+    // ASSERT
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
+
+test('returns [4,5,6] for range (4) (6)', () => {
+    // ARRANGE
+    const expected = [4, 5, 6];
+    // ACT
+    let result = Range(4);
+    result = result(6);
+    // ASSERT
+    expect(result).toEqual(expect.arrayContaining(expected));
+});
